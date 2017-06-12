2de8dc07a513202f280b6ca5bde0768891d4e05a
hoang hiep <hiep.tranhoang@homedirect.com.vn>
2017-06-12 13:32:46 +0700

    login page and register page

---
+++ web-travel/src/main/java/com/hoanghiep/webtravel/controller/AdminController.java
@@ -0,0 +1,27 @@
+package com.hoanghiep.webtravel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by hoanghiep on 12/06/2017.
+ */
+@Controller
+public class AdminController {
+
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView("login");
+        mav.addObject("title", "Login - Admin");
+        return mav;
+    }
+
+    @RequestMapping("/register")
+    public ModelAndView register(){
+        ModelAndView mav = new ModelAndView("register");
+        mav.addObject("title", "Đăng ký - Quản trị");
+        return mav;
+    }
+
+}
